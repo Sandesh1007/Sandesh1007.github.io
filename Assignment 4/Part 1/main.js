67270d8b952e3ec38314efe4813d5c1e33320578
Sandesh1007 <sandesh.pyakurel@dcmail.ca>
2024-08-02 15:37:06 -0400

    class ball added
    class ball added

--- Assignment 4/Part 1/main.js
+++ Assignment 4/Part 1/main.js
@@ -3,40 +3,40 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas']
-let insertY = [ 'the soup kitchen','Disneyland','the White House']
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away']
-
 function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
 }
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
+const insertY = ['the soup kitchen','Disneyland','the White House'];
+const insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
 function result() {
     let newStory = storyText;
-    let xItem = randomValueFromArray(insertX);
-    let yItem = randomValueFromArray(insertY);
-    let zItem = randomValueFromArray(insertZ);
-    newStory = newStory.replace(':insertx:',xItem).replace(':insertx:',xItem).replace(':inserty:',yItem).replace(':inserty:',yItem).replace(':insertz',zItem);
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+    newStory = newStory.replaceAll(':insertx:',xItem);
+    newStory = newStory.replaceAll(':inserty:',yItem);
+    newStory = newStory.replaceAll(':insertz:',zItem);
 
   if(customName.value !== '') {
-    let name = customName.value;
-    newStory = newStory.replace('Bob',name);
+    const name = customName.value;
+    newStory = newStory.replaceAll('Bob',name);
   }
 
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300*0.07).toString() + 'stone';
-    let temperature = Math.round((94-32)/1.8).toString() + 'centigrade';
-    newStory =  newStory.replace('94 farenheit',temperature).replace('300 pounds',weight);
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature = `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory =  newStory.replaceAll('94 fahrenheit',temperature);
+    newStory =  newStory.replaceAll('300 pounds',weight);
   }
 
-  story.textContent = nweStory;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
 
-randomize.addEventListener('click',result);
